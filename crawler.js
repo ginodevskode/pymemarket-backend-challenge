b873303b25a520de1a5ccfd53adafc579281b1ad
Gino Boca <gnoboca@gmail.com>
2024-04-26 15:28:42 -0300

    CC-2: Get response data

--- crawler.js
+++ crawler.js
@@ -1,4 +1,6 @@
 const { program } = require("commander");
+const axios = require("axios");
+const unfluff = require("unfluff");
 
 program
   .requiredOption("--url <url>", "Initial URL to start crawling from")
@@ -6,8 +8,23 @@ program
 
 program.parse(process.argv);
 const options = program.opts();
-const results = {
-  pages: [],
-};
 
-console.log(options);
+async function crawl(url, depth) {
+  try {
+    const response = await axios.get(url);
+    console.log("response", unfluff(response.data));
+    const { title, text, links } = unfluff(response.data);
+    console.log(title, text, links);
+  } catch (error) {
+    console.error(`Error crawling ${url}: ${error.message}`);
+  }
+}
+
+async function main() {
+  await crawl(options.url, parseInt(options.maxdist));
+  // ToDo: save results
+}
+
+main().catch((error) => {
+  console.error(`Error: ${error.message}`);
+});
